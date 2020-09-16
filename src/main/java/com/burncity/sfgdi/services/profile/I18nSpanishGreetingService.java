17747b96232cd1b3016dde64054f36e0aaf7d99f
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-09-16 13:34:57 +0200

    Added "default" profile

--- src/main/java/com/burncity/sfgdi/services/profile/I18nSpanishGreetingService.java
+++ src/main/java/com/burncity/sfgdi/services/profile/I18nSpanishGreetingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.burncity.sfgdi.services.GreetingService;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
 
